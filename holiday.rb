0867669319d291844ac85dcd4c95a32139d1cdd1
Ezra Ellette <ezrasellette@gmail.com>
2020-11-18 14:38:01 -0600

    added more stuffs

--- holiday.rb
+++ holiday.rb
@@ -1,7 +1,9 @@
 require 'erubis'
 require 'sinatra'
+require 'sinatra/reloader'
 require 'sinatra/json'
 require 'bcrypt'
+require 'json'
 
 configure do
   enable :sessions
@@ -9,13 +11,53 @@ configure do
   set :erb, :escape_html => true
 end
 
+def escape_html(html)
+  Rack::Utils.escape_html(html)
+end
+
+def user_list
+  # user_list = YAML.load_file(File.join(data_path, 'users.yml') )
+  # user_list ? user_list : {}
+  ["Rodney", "Ezra"]
+end
+
+def verify_credentials(username, password)
+  # return false if user_list[username].nil? || password.empty?
+  # BCrypt::Password.new(user_list[username]) == password
+
+  user_list.include?(username) && password == 'password'
+end
+
+def signed_in?
+  return false unless user_list
+  user_list.include?(session[:username])
+end
+
 get '/' do
   send_file 'index.html'
 end
 
+post '/add/ingredient' do
+  request.body.rewind
+  # puts(JSON.parse(request.body.read).to_json)
+  json JSON.parse(request.body.read).to_json
+end
 
-post '/' do
-  obj = JSON.parse request.body.read
+post '/login' do
+  username = escape_html(params[:username])
+  password = escape_html(params[:password])
 
-  json(obj, :encoder => :to_json, :content_type => :json)
+  logged_in = verify_credentials(username, password)
+
+  response = {}
+
+  response['loggedIn'] = logged_in
+  response['person'] = {'name' => 'Rodney', 'specialIngredients' => []}
+  response['flash'] = {'message' => 'Logged in!', 'action' => 'Happy Thanksgiving week!'}
+  json response.to_json
+end
+
+
+post '/create/account' do
+#
 end
