6041e8e3d7202e88ca8967a0f8050740a97395fc
Ezra Ellette <ezrasellette@gmail.com>
2020-11-20 13:48:45 -0600

    more stuff

--- database_persistence.rb
+++ database_persistence.rb
@@ -1,5 +1,5 @@
 require 'pg'
-
+require 'pry'
 class DatabasePersistence
 
   def initialize
@@ -25,14 +25,18 @@ class DatabasePersistence
     query(sql, username).map{ |tuple| tuple['userpassword']}[0]
   end
 
-  def get_user_info(username)
+  def get_ingredients(username)
     sql = <<~SQL
-      SELECT ingredients.name FROM ingredients JOIN user_ingredients
+      SELECT DISTINCT ingredients.name FROM ingredients JOIN user_ingredients
       ON user_ingredients.ingredientid = ingredients.id JOIN users
       ON users.id = user_ingredients.userid
-      WHERE users.username = '#{username}'
+      WHERE users.username = $1
     ;
     SQL
+    res = query(sql, username)
+    arr = []
+    res.each { |i| arr.push(i) }
+    arr
   end
 
   def get_user_id(user)
@@ -40,17 +44,81 @@ class DatabasePersistence
       SELECT id FROM users WHERE username = $1;
     SQL
 
-    query(sql)[0];
+    query(sql, user)[0]['id'];
   end
 
-  def add_ingredient(ingredient, id)
+  def get_ingredient_id(ingredient)
+    sql = <<~SQL
+      SELECT id FROM ingredients WHERE name = $1;
+    SQL
+
+    p query(sql, ingredient)[0]["id"].to_i
+  end
+
+  def add_ingredient_to_user(user_id, ingredient_id)
+    sql = <<~SQL
+      INSERT INTO user_ingredients (userid, ingredientid) VALUES ($1, $2);
+    SQL
+
+    query(sql, user_id, ingredient_id)
+  end
+
+  def ingredient_exists?(ingredient)
+    sql = <<~SQL
+      SELECT name FROM ingredients WHERE name = $1;
+    SQL
+
+    query(sql, ingredient.upcase).count > 0
+  end
+
+  def add_ingredient(ingredient, userid)
     ingredient.upcase!
-    # @db.exec()
+
     sql = <<~SQL
       INSERT INTO ingredients (name) VALUES ($1);
-
-      INSERT INTO user_ingredients (userid, ingredientid)
-              VALUES (id, );
     SQL
+
+    unless ingredient_exists?(ingredient)
+      query(sql, ingredient)
+    end
+
+    ingredient_id = get_ingredient_id(ingredient)
+
+    add_ingredient_to_user(userid, ingredient_id);
+  end
+
+  def get_random_ingredients(username)
+    sql = <<~SQL
+      SELECT ingredients.name FROM ingredients WHERE ingredients.id NOT IN (
+        SELECT ingredients.id FROM ingredients JOIN user_ingredients
+        ON user_ingredients.ingredientid = ingredients.id JOIN users
+        ON users.id = user_ingredients.userid
+        WHERE users.username = $1
+      ) ORDER BY RANDOM() LIMIT 5;
+    SQL
+
+    p random_ingredients = query(sql, username)
+    user_id = get_user_id(username).to_i
+    random_ingredients.each do |ingredient|
+      ingredient_id = get_ingredient_id(ingredient["name"])
+
+      add_ingredient_to_user(user_id, ingredient_id)
+    end
+  end
+
+  def username_available?(username)
+    sql = <<~SQL
+      SELECT (username) FROM users WHERE username = $1
+    SQL
+
+    query(sql, username).ntuples == 0
+  end
+
+  def add_user(username, password)
+    sql = <<~SQL
+      INSERT INTO users (username, userPassword) VALUES ($1, $2);
+    SQL
+
+    query(sql, username, password)
   end
 end
--- holiday.rb
+++ holiday.rb
@@ -17,12 +17,17 @@ configure(:development) do
   also_reload "database_persistence.rb"
 end
 
+def clear_flash(session)
+  session[:current].delete(:flash) if session[:current] && session[:current][:flash]
+end
+
 before do
   @storage = DatabasePersistence.new()
 end
 
 after do
   @storage.disconnect
+  clear_flash(session) if request.path_info[-1] == '/'
 end
 
 def escape_html(html)
@@ -41,7 +46,8 @@ def signed_in?
 end
 
 get '/' do
-  send_file 'index.html'
+  # send_file 'index.html'
+  erb :index
 end
 
 # post '/add/ingredient' do
@@ -54,43 +60,89 @@ post '/login' do
   username = escape_html(params[:username])
   password = escape_html(params[:password])
 
+  session[:current] ||= {}
+
   logged_in = verify_credentials(username, password)
 
   return unless logged_in
-    # session[:user_id] = @storage.get_user_id(username) || nil
 
+  session[:user_id] = @storage.get_user_id(username) || nil
+  # p session[:user_id]
+  session[:username] = username
   response = {}
 
-  response['loggedIn'] = logged_in
-
-  response['person'] = {'name' => username, 'selectedIngredients' => []}
-  response['flash'] = {'message' => 'Logged in!', 'action' => 'Happy Thanksgiving week!'}
+  session[:current][:loggedIn] = logged_in
+  session[:current][:person] = {'name' => username, :selectedIngredients => @storage.get_ingredients(username)}
+  # response['person'] = {'name' => username, 'selectedIngredients' => []}
+  session[:current][:flash] = {'message' => 'Logged in!', 'action' => 'Happy Thanksgiving week!'}
 
   # @storage.add_ingredient(params[:add_ingredient], session[:user_id])
 
-  json response.to_json
+  # json response.to_json
+  redirect '/'
 end
 
 post '/add/ingredient' do
   ingredient_name = params[:ingredient]
   response = {}
-  # response['person'] = {'selectedIngredients' => [{'name' => ingredient_name}]}
-  response['ingredient'] = {"name" => ingredient_name};
-  response['flash'] = {'message' => 'Ingredient(s) added!', 'action' => 'You can add more!'}
-  # response[:ingredient] = ingredient_name
-  json response.to_json
-  # redirect '/'
-  # @storage.add_ingredient(ingredient_name, session[:user_id])
+  if @storage.get_ingredients(session[:username]).count >= 5
+    session[:current][:flash] = {'message' => 'Max ingredients selected!', 'action' => 'If you haven\'t added random ingredients, do that!'}
+    redirect '/'
+  end
+
+  session[:current] ||= {}
+
+  if @storage.add_ingredient(ingredient_name, session[:user_id])
+    session[:current][:person][:selectedIngredients] = @storage.get_ingredients(session[:username])
+    # response['ingredient'] = {"name" => ingredient_name};
+    session[:current][:flash] = {'message' => 'Ingredient added!', 'action' => 'You can add more!'}
+  else
+    session[:flash] = {'message' => 'Ingredient could not be added!', 'action' => 'You can try again!'}
+  end
+
+  # json response.to_json
+  redirect '/'
+  # erb :index
 end
 
 post '/create/account' do
-#
+  username = params[:username]
+  password = params[:password]
+  password2 = params[:password2]
+
+  session[:current] ||= {}
+
+  if !@storage.username_available?(username)
+
+    session[:current][:flash] = {message: "Username is unavailable!", action: "try a different username."}
+    redirect '/'
+  end
+
+  if password != password2
+    session[:current][:flash] = {message: "Passwords must match!", action: "try again"}
+    redirect '/'
+  end
+
+  @storage.add_user(username, BCrypt::Password.create(password))
+
+  session[:current][:flash] = {message: 'You may now log in!', action: ''}
+  redirect '/'
+end
+
+get '/logout' do
+  session.clear
+
+  redirect '/'
 end
 
 post '/add/random/ingredients' do
   response = {}
+  session[:current] ||= {}
 
-  response['randomIngredients'] = [{name: "potato"}, {name: "yam"}, {name: "cranberry"}]
+  session[:current][:flash] = {message:" here are your random ingredients!", action: "now, you can make a dish"};
 
-  json response.to_json
+  @storage.get_random_ingredients(session[:username])
+
+  session[:current][:person][:selectedIngredients] = @storage.get_ingredients(session[:username])
+  redirect '/'
 end
